ae7849a8263ca4815a1f67b5cd21562e977f5327
Bryanoxx <contact@bryanparasote.fr>
2021-03-06 15:41:08 +0000

    Les conditions

--- app.js
+++ app.js
@@ -3,7 +3,39 @@ Vue.createApp({
     return {
       title: 'Vue<strong>Tracker</strong>',
       logoSource: 'https://cdn.svgporn.com/logos/vue.svg',
-      taskname: ''
+      taskname: '',
+      isTaskInProgress: false,
+      startTime: null,
+      errorMsg: null
+    }
+  },
+  methods: {
+    startTask () {
+      // Vérifications
+      if (this.taskname.length === 0) {
+        this.errorMsg = "Le nom d'une tâche ne peut pas être vide"
+        return
+      } else if (this.isTaskInProgress) {
+        this.errorMsg = 'Une tâche est déjà en cours'
+        return
+      } else {
+        this.errorMsg = null
+      }
+
+      // Début de la tâche
+      this.isTaskInProgress = true
+      this.startTime = Date.now()
+    },
+    stopTask () {
+      // Vérifications
+      if (!this.isTaskInProgress) {
+        this.errorMsg = "Aucune tâche n'est en cours"
+        return
+      }
+
+      // Fin de la tâche
+      this.isTaskInProgress = false
+      this.errorMsg = null
     }
   }
 }).mount('#app')
