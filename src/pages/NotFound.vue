2ce94ff56f0a232ca1871e1d08f4ca33811233f0
Bryanoxx <contact@bryanparasote.fr>
2021-04-21 10:45:18 +0100

    Refactoring de NotFound via la Composition API

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-    <h1>Page introuvable</h1>
-    <h2 v-if="$route.params.wrongPath">"{{ $route.params.wrongPath }}"</h2>
-    <el-button @click="$router.back()" type="primary" round>
+    <h1>{{ title }}</h1>
+    <h2 v-if="wrongPath">"{{ wrongPath }}"</h2>
+    <el-button @click="goToPreviousPage" type="primary" round>
       Revenir en arrière
     </el-button>
   </div>
 </template>
 
 <script>
+  import { reactive, toRefs } from 'vue'
+  import { useRouter, useRoute } from 'vue-router'
   export default {
-    
+    setup () {
+
+      const router = useRouter()
+      const route = useRoute()
+
+      const state = reactive({ 
+        wrongPath: route.params.wrongPath,
+        title: 'Page introuvable'
+      })
+
+      const goToPreviousPage = function() {
+        router.back()
+      }
+
+      return {
+        ...toRefs(state),
+        goToPreviousPage
+      }
+
+    }
   }
 </script>
 
